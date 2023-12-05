0dd50af1bddbf1180b965f76717847d8f7378771
Michael Wu <mw10013@gmail.com>
2023-12-04 21:38:45 -0500

    tailwind

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,8 +9,8 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+    <div className="max-w-lg mx-auto mt-8">
+      <h1 className="text-xl font-bold">Welcome to Remix</h1>
       <ul>
         <li>
           <a
